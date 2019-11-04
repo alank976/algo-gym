e3ef6f87a105c4ce60f448f381577a1e35880d3d
alank <alankan976@gmail.com>
2019-11-04 22:50:18 +0800

    my dummy solution

--- common-denomiator/src/lib.rs
+++ common-denomiator/src/lib.rs
@@ -1,7 +1,51 @@
+/// https://www.codewars.com/kata/54d7660d2daf68c619000d95/train/rust
+fn convert_fracts(l: Vec<(i64, i64)>) -> Vec<(i64, i64)> {
+    let divisors: Vec<i64> = l.iter()
+        .map(|(_, d)| *d)
+        .collect();
+    let lcm_divisors = divisors.iter().fold(1, |acc, &d| lcm(acc, d));
+
+    let before_simplified: Vec<(i64, i64)> = l.into_iter()
+        .map(|(num, denom)| (num * lcm_divisors / denom, lcm_divisors))
+        .collect();
+    let common_factor = before_simplified.iter()
+        .map(|(num, denom)| gcd(*num, *denom))
+        .fold(0, |acc, g| gcd(acc, g));
+    before_simplified.into_iter()
+        .map(|(num, denom)| (num / common_factor, denom / common_factor))
+        .collect()
+}
+
+fn lcm(a: i64, b: i64) -> i64 {
+    a * b / gcd(a, b)
+}
+
+fn gcd(a: i64, b: i64) -> i64 {
+    let mut remainder;
+    let mut tmp_a = a;
+    let mut tmp_b = b;
+
+    while {
+        remainder = tmp_a % tmp_b;
+        tmp_a = tmp_b;
+        tmp_b = remainder;
+        remainder != 0
+    } {}
+    tmp_a
+}
+
+
 #[cfg(test)]
 mod tests {
+    use crate::convert_fracts;
+
+    fn testing(l: Vec<(i64, i64)>, exp: Vec<(i64, i64)>) -> () {
+        assert_eq!(convert_fracts(l), exp)
+    }
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn basics_convert_fracts() {
+        testing(vec![(69, 130), (87, 1310), (3, 4)], vec![(18078, 34060), (2262, 34060), (25545, 34060)]);
+        testing(vec![(690, 1300), (87, 1310), (30, 40)], vec![(18078, 34060), (2262, 34060), (25545, 34060)]);
     }
 }
