e669c7a6cefd7eb7acb19644d4b9a51beeadb060
alank <alankan976@gmail.com>
2020-01-22 01:22:06 +0800

    setup basic kotlin project

---
+++ leetcode/leetcode-kotlin/src/main/kotlin/io/alank/leetcode/App.kt
@@ -0,0 +1,5 @@
+package io.alank.leetcode
+
+class App {
+
+}
---
+++ leetcode/leetcode-kotlin/src/main/kotlin/io/alank/leetcode/ReorganizeString.kt
@@ -0,0 +1,9 @@
+package io.alank.leetcode
+
+//https://leetcode.com/problems/reorganize-string/description/
+
+class Solution {
+    fun reorganizeString(S: String): String {
+        return ""
+    }
+}
