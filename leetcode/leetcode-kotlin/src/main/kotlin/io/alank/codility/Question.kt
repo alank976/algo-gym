66f42ecf06af271ba3d6cd2f02afc0acfd1b35b8
alank <alankan976@gmail.com>
2020-02-23 23:22:11 +0800

    git ignore fix

---
+++ leetcode/leetcode-kotlin/src/main/kotlin/io/alank/codility/Question.kt
@@ -0,0 +1,61 @@
+package io.alank.codility
+
+class Question {
+
+    fun solution(a: IntArray): Int {
+        val max = a.max() ?: 0
+        val set = a.toSet()
+        return (1..max + 1).firstOrNull { it !in set } ?: 1
+    }
+
+    data class FoldState(var maxDistance: Int = 0, var prevIndex: Int)
+
+    fun solution1(a: IntArray): Int {
+        val adjacentIndices = a
+                .withIndex()
+                .sortedBy { (_, v) -> v }
+                .groupBy({ (_, v) -> v }) { (i, _) -> i }
+                .mapValues { (_, indicesWithSameValue) -> indicesWithSameValue.max()!! }
+                .values
+        val firstIndex = adjacentIndices.first()
+        return adjacentIndices.fold(FoldState(prevIndex = firstIndex)) { state, i ->
+            val distance = i - state.prevIndex
+            if (distance > state.maxDistance) {
+                state.maxDistance = distance
+            }
+            state.prevIndex = i
+            state
+        }.maxDistance
+    }
+
+
+    fun solution2(a: IntArray): Int {
+        var i = 0
+        var maxSliceLength = 0
+
+        while (i < a.size - 1) {
+            var j: Int = i + 1
+            var firstSeenTheOtherValueAt: Int? = null
+
+            while (j < a.size && (i..j).map { a[it] }.toSet().size <= 2) {
+                if (firstSeenTheOtherValueAt == null && a[j] != a[i]) {
+                    firstSeenTheOtherValueAt = j
+                }
+                j++
+            }
+            val sliceLength = j - i
+            if (sliceLength > maxSliceLength) {
+                maxSliceLength = sliceLength
+            }
+            i = firstSeenTheOtherValueAt ?: i + 1
+            firstSeenTheOtherValueAt = null
+        }
+        return maxSliceLength
+    }
+
+
+    // Must be sorted before
+    fun IntArray.isConsecutive(): Boolean = last() - first() + 1 == size
+
+
+}
---
+++ leetcode/leetcode-kotlin/src/test/kotlin/io/alank/codility/QuestionTest.kt
@@ -0,0 +1,39 @@
+package io.alank.codility
+
+import io.kotlintest.data.forall
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+import io.kotlintest.tables.row
+
+class QuestionTest : StringSpec() {
+
+    init {
+//        "demo works" {
+//            forall(
+//                    row(intArrayOf(1, 3, 6, 4, 1, 2), 5)
+//            ) { input, expected ->
+//                Question().solution(input) shouldBe expected
+//            }
+//        }
+
+        "q1 works" {
+            forall(
+                    row(intArrayOf(1, 4, 7, 3, 3, 5), 4)
+            ) { input, expected ->
+                Question().solution1(input) shouldBe expected
+            }
+        }
+
+        "q2 helper test" {
+            forall(
+                    row(intArrayOf(4,4), 2)/*,
+                    row(intArrayOf(1,2,3,2), 3),
+                    row(intArrayOf(0,5,4,4,5,12), 4),
+                    row(intArrayOf(0,5,4,4,5,12), 4),*/
+            ) { input, expected ->
+                Question().solution2(input) shouldBe expected
+            }
+        }
+    }
+
+}
