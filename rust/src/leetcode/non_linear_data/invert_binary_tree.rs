e91efac3f24998416a6fd7a61a4462b7f52519a9
alank <alankan976@gmail.com>
2020-05-07 23:16:27 +0800

    invert binary tree

---
+++ rust/src/leetcode/non_linear_data/invert_binary_tree.rs
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/invert-binary-tree/
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+#[allow(dead_code)]
+struct Solution;
+// -----------------------------------------------------------------------
+use std::cell::RefCell;
+use std::rc::Rc;
+#[allow(dead_code)]
+impl Solution {
+    pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::swap_children(root.as_ref());
+        root
+    }
+
+    fn swap_children(root: Option<&Rc<RefCell<TreeNode>>>) {
+        if let Some(r) = root {
+            let mut node = r.borrow_mut();
+            let temp = node.left.as_ref().map(|x| x.clone());
+            node.left = node.right.as_ref().map(|x| x.clone());
+            node.right = temp;
+            Self::swap_children(node.left.as_ref());
+            Self::swap_children(node.right.as_ref());
+        }
+    }
+}
--- rust/src/leetcode/non_linear_data/mod.rs
+++ rust/src/leetcode/non_linear_data/mod.rs
@@ -1,1 +1,2 @@
 mod validate_binary_search_tree;
+mod invert_binary_tree;
