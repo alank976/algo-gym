e148097d21357d615caebce1d4b97dcb80eb6abe
alank <alankan976@gmail.com>
2020-07-26 15:33:18 +0800

    optimize with vector rather than hashset

--- rust/src/leetcode/non_linear_data/course_schedule.rs
+++ rust/src/leetcode/non_linear_data/course_schedule.rs
@@ -5,65 +5,58 @@
 #[allow(dead_code)]
 struct Solution;
 // --------------------------------
-use std::collections::{HashMap, HashSet};
+const COURSE: usize = 0;
+const PREREQUISITE: usize = 1;
 
 #[allow(dead_code)]
 impl Solution {
     pub fn can_finish(num_courses: i32, prerequisites: Vec<Vec<i32>>) -> bool {
-        let prereqs_by_subjects = Self::group_by_subject(prerequisites);
-        let mut subjects_without_cycle: HashSet<i32> = HashSet::new();
-        let mut seen_subjects: HashSet<i32> = HashSet::new();
+        let num_courses = num_courses as usize;
+        let prereqs_by_course = Self::prereqs_by_course(prerequisites, num_courses);
+        let mut is_not_cyclic_by_course: Vec<bool> = vec![false; num_courses];
+        let mut is_seen_by_course: Vec<bool> = vec![false; num_courses];
 
-        !prereqs_by_subjects.keys().any(|&subj| {
-            let a = Self::is_cycle(
-                &mut seen_subjects,
-                subj,
-                &mut subjects_without_cycle,
-                &prereqs_by_subjects,
-            );
-            a
+        !(0..num_courses).any(|course| {
+            Self::is_cycle(
+                &mut is_seen_by_course,
+                course,
+                &mut is_not_cyclic_by_course,
+                &prereqs_by_course,
+            )
         })
     }
 
     fn is_cycle(
-        seen_subjects: &mut HashSet<i32>,
-        subj: i32,
-        subjects_without_cycle: &mut HashSet<i32>,
-        prereqs_by_subjects: &HashMap<i32, Vec<i32>>,
+        is_seen: &mut Vec<bool>,
+        course: usize,
+        is_not_cyclic: &mut Vec<bool>,
+        prereqs_by_course: &Vec<Vec<usize>>,
     ) -> bool {
-        if subjects_without_cycle.contains(&subj) {
+        if is_not_cyclic[course] {
             return false;
         }
-        if seen_subjects.contains(&subj) {
+        if is_seen[course] {
             return true;
         }
-        seen_subjects.insert(subj);
-        let is_cycle = prereqs_by_subjects.get(&subj).map_or(false, |prereqs| {
-            prereqs.iter().fold(false, |is_prev_cycle, &p| {
-                is_prev_cycle
-                    || Self::is_cycle(
-                        seen_subjects,
-                        p,
-                        subjects_without_cycle,
-                        prereqs_by_subjects,
-                    )
-            })
+        is_seen[course] = true;
+        let is_cycle = prereqs_by_course[course].iter().any(|&prereq_course| {
+            Self::is_cycle(is_seen, prereq_course, is_not_cyclic, prereqs_by_course)
         });
         if !is_cycle {
-            subjects_without_cycle.insert(subj);
+            is_not_cyclic[course] = true;
         }
         is_cycle
     }
 
-    fn group_by_subject(prerequisites: Vec<Vec<i32>>) -> HashMap<i32, Vec<i32>> {
-        prerequisites.into_iter().fold(HashMap::new(), |mut m, p| {
-            let subj = p[0];
-            let prereq = p[1]; // given it's a pair
-            m.entry(subj)
-                .and_modify(|e| e.push(prereq))
-                .or_insert(vec![prereq]);
-            m
-        })
+    fn prereqs_by_course(prerequisites: Vec<Vec<i32>>, num_courses: usize) -> Vec<Vec<usize>> {
+        prerequisites
+            .into_iter()
+            .fold(vec![Vec::new(); num_courses], |mut vv, p| {
+                let course = p[COURSE] as usize;
+                let prereq = p[PREREQUISITE] as usize;
+                vv[course].push(prereq);
+                vv
+            })
     }
 }
 
@@ -79,7 +72,7 @@ mod test_non_linear_data {
             (true, (1, vec![])),
             (
                 false,
-                (1, vec![vec![0, 1], vec![1, 2], vec![1, 0], vec![2, 3]]),
+                (4, vec![vec![0, 1], vec![1, 2], vec![1, 0], vec![2, 3]]),
             ),
             (false, (3, vec![vec![1, 0], vec![2, 0], vec![0, 1]])),
             (false, (3, vec![vec![0, 2], vec![1, 2], vec![2, 0]])),
